ceb245e807880a38019743b5c2e19a2884d03557
Alan Nascimento <alan.silva.n@hotmail.com>
2020-01-07 23:26:06 -0300

    Adding GraphQL and user schema

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { GraphQLServer } from 'graphql-yoga';
+import { resolve } from 'path';
+
+const server = new GraphQLServer({
+  typeDefs: resolve(__dirname, 'schema.graphql'),
+  resolvers: {}
+});
+
+server.start();
